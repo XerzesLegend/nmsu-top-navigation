8fec19585a91d55b3c310e9fcb849a69f16e8394
XerzesLegend <87173826+XerzesLegend@users.noreply.github.com>
2022-02-03 16:04:54 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 addDropDownFunctionality();
-
-
+navBarButton();
+goBackButton();
 
 
 function addDropDownFunctionality(){
+    const main = document.querySelector('.mainContainer');
     const menu = document.querySelector('#menu');
     let btns = menu.querySelectorAll('a');
     let menus = document.querySelectorAll('div.menu');
@@ -13,6 +14,7 @@ function addDropDownFunctionality(){
     btns.forEach(btn => {
         let index = btns.indexOf(btn);
         btn.addEventListener('click', function(e){
+            e.preventDefault();
             btns.forEach(btn =>{
                 let temp = btns.indexOf(btn);
                 if(index == temp){
@@ -22,6 +24,7 @@ function addDropDownFunctionality(){
                 }
             });
             menus[index].classList.toggle('items-hide');
+            main.classList.toggle('containerHide');
         });
         window.addEventListener('click', function(e){   
             if (document.getElementById('maincontainer').contains(e.target)){
@@ -30,12 +33,35 @@ function addDropDownFunctionality(){
               menus.forEach(men => {
                 men.classList.add('items-hide');
               });
+              document.querySelector('.mainContainer').classList.remove('containerHide');
             }
           });
     });
 
-}
+};
 
-function closeDropDown(){
+function navBarButton(){
+    const container = document.querySelector('.mainContainer');
+    const parentContainer = document.querySelector('#maincontainer');
+    const button = document.querySelector('button.navbar-toggle');
+    button.addEventListener('click', function(){
+        container.classList.toggle('containerHide');
+        parentContainer.classList.toggle('containerHide');
+    });
+};
 
-}
+function goBackButton(){
+    const main = document.querySelector('.mainContainer');
+    let btns = document.querySelectorAll('a.goBack');
+    btns = Array.from(btns);
+    btns.forEach(btn => {
+        btn.addEventListener('click', function(){
+            let parent = btn.parentElement.parentElement.parentElement;
+            main.classList.toggle('containerHide');
+            parent.classList.toggle('items-hide');
+        });
+    });
+    
+    
+};
+
